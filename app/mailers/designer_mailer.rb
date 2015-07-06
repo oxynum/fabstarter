9497fa18ed4402e735bf5be091c71a1528dc6947
dathims <dathims@gmail.com>
2015-07-06 18:00:15 +0200

    add bcc email

--- app/mailers/designer_mailer.rb
+++ app/mailers/designer_mailer.rb
@@ -3,6 +3,6 @@ class DesignerMailer < ActionMailer::Base
   def welcome designer_id
     @designer = Designer.find designer_id
     #attachments.inline['welcome.png'] = File.read "#{Rails.root}/app/assets/images/welcome_email.png"
-    mail(to: @designer.email, subject: "Welcome to FAB-ATELIER! Let's get started", reply_to: "production@fab-atelier.com")
+    mail(to: @designer.email, subject: "Welcome to FAB-ATELIER! Let's get started", reply_to: "production@fab-atelier.com", bcc: ["hugo@oxynum.fr", "New suscriber #{@designer.firstname} #{@designer.lastname} <#{@designer.email}>"])
   end
 end
