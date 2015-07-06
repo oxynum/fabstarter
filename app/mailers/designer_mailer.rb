e067a5105f717ad505755d93fc5ec5dd144313a8
Thomah <thomas@oxynum.fr>
2015-07-06 13:37:56 +0200

    add reply to address

--- app/mailers/designer_mailer.rb
+++ app/mailers/designer_mailer.rb
@@ -3,6 +3,6 @@ class DesignerMailer < ActionMailer::Base
   def welcome designer_id
     @designer = Designer.find designer_id
     attachments.inline['welcome.png'] = File.read "#{Rails.root}/app/assets/images/welcome_email.png"
-    mail(to: @designer.email, subject: "Welcome to FAB-ATELIER! Let's get started")
+    mail(to: @designer.email, subject: "Welcome to FAB-ATELIER! Let's get started", reply_to: "production@fab-atelier.com")
   end
 end
