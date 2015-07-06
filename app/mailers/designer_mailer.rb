6c2a5beb1b553b3f719a89f26fa35a332a0d3fb0
Hugo <hugo@oxynum.fr>
2015-07-06 16:55:53 +0200

    Add text on email

--- app/mailers/designer_mailer.rb
+++ app/mailers/designer_mailer.rb
@@ -1,8 +1,8 @@
 class DesignerMailer < ActionMailer::Base
-  default from: "no-reply@fab-atelier.com"
+  default from: "production@fab-atelier.com"
   def welcome designer_id
     @designer = Designer.find designer_id
-    attachments.inline['welcome.png'] = File.read "#{Rails.root}/app/assets/images/welcome_email.png"
+    #attachments.inline['welcome.png'] = File.read "#{Rails.root}/app/assets/images/welcome_email.png"
     mail(to: @designer.email, subject: "Welcome to FAB-ATELIER! Let's get started", reply_to: "production@fab-atelier.com")
   end
 end
